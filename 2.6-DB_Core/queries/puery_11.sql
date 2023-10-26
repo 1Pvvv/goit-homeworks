64a39f54635292ec8ed16d79d2e88c387be433e7
1Pvvv <potap.iv@pm.me>
2023-10-26 19:41:38 +0300

    fix bugs

--- 2.6-DB_Core/queries/puery_11.sql
+++ 2.6-DB_Core/queries/puery_11.sql
@@ -7,4 +7,4 @@ JOIN grades g ON st.student_id = g.student_id
 JOIN subjects s ON g.subject_id = s.subject_id
 JOIN teachers t ON s.teacher_id = t.teacher_id
 WHERE st.student_id = 1
-  AND t.teacher_id = 1;
+  AND t.teacher_id = 2;
--- 2.6-DB_Core/queries/puery_5.sql
+++ 2.6-DB_Core/queries/puery_5.sql
@@ -2,4 +2,4 @@
 SELECT s.subject_name
 FROM subjects s
 JOIN teachers t ON s.teacher_id = t.teacher_id
-WHERE t.teacher_id = 1;
+WHERE t.teacher_id = 3;
